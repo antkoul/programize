b64501cda3d961615afad86da0ad6f5fa85c5b5e
antkoul <antkoy_ee@hotmail.com>
2017-07-06 16:16:29 +0300

    app.js

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const bodyParser= require('body-parser')
+const app = express()
+
+
+
+app.post('/items', (req, res) => {	
+	res.setHeader('Location', 'http://localhost:4040/items/1') 	
+	res.status(201).send('The TODOs were added.')
+	//console.log('201 - The TODO was added')
+})
+
+app.put('/items/:id', (req, res) => {
+	//console.log('204 - The TODOs were updated.')
+  	res.status(204).end()
+})
+
+app.delete('/items/:id', (req, res) => {
+	console.log('204 - The TODOs were deleted.')
+  	res.status(204).send('The TODOs were deleted.')
+})
+
+app.get('/items', (req, res) => {
+	//res.setHeader('Content-Type', 'application/json');
+	//res.sendFile('/Users/antonioskoulouris/Desktop/Programize/todolist/index.html') //dokimastiko
+	console.log('200 - The TODOs were returned.')
+	res.status(200).json({"items":
+			[{
+			"title": "Buy ice-cream",
+      	  		"done": true
+    			},
+    			{
+      			"title": "Learn React",
+      			"done": false
+    			}]
+		})
+})
+
+app.listen(4040, () => {
+  console.log('app.js listening on port 4040!')
+})
