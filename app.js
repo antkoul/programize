7e3f98631594c754b19398746c86c1d5a7b70bb2
antkoul <antkoy_ee@hotmail.com>
2017-07-07 11:25:33 +0300

    Third commit. Clean the code

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
-app.post('/items', (req, res) => {	
-  res.setHeader('Location', 'http://localhost:4040/items/1') 	
-  res.status(201).send('The TODOs were added.')
-})
+app.post('/items', (req, res) => {
+  res.setHeader('Location', 'http://localhost:4040/items/1') 	;
+  res.status(201).send('The TODOs were added.');
+});
 
 app.put('/items/:id', (req, res) => {
-  res.status(204).end()
-})
+  res.status(204).end();
+});
 
 app.delete('/items/:id', (req, res) => {
-  console.log('204 - The TODOs were deleted.')
-  res.status(204).send('The TODOs were deleted.')
-})
+  console.log('204 - The TODOs were deleted.');
+  res.status(204).send('The TODOs were deleted.');
+});
 
 app.get('/items', (req, res) => {
-  console.log('200 - The TODOs were returned.')
+  console.log('200 - The TODOs were returned.');
   res.status(200).json({"items":
     [{
     "title": "Buy ice-cream",
@@ -26,9 +26,9 @@ app.get('/items', (req, res) => {
     "title": "Learn React",
     "done": false
     }]
-  })
-})
+  });
+});
 
 app.listen(4040, () => {
   console.log('app.js listening on port 4040!')
-})
+});
