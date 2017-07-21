0dfa93a47fa1070a2a0236af4fd9591a5f4ce5b6
antkoul <antkoy_ee@hotmail.com>
2017-07-21 15:32:20 +0300

    2nd commit for task3

--- todolist/task3/app.js
+++ todolist/task3/app.js
@@ -4,7 +4,7 @@ var app = express();
 app.use(bodyparser.json());
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
-mongoose.Promise = global.Promise; //
+mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/testdb');
 
 var db = mongoose.connection;
@@ -21,20 +21,52 @@ var todoSchema = mongoose.Schema({
 
 var todoModel = mongoose.model('todoModel', todoSchema);
 
+//add a todo
+app.post('/items', (req, res) => {
+  var addTodo = new todoModel({ name: req.body.name, completed: false, addedAt: new Date() });
+  addTodo.save()
+    .then( function (addTodo){
+    res.setHeader('Location', 'http://localhost:4040/items/1');
+    res.status(201).send('The TODOs were added.');
+  })
+  .catch(function(err) {
+    console.log(err);
+  });
+});
+
+//update a todo
+app.put('/items/:id', (req, res) => {
+  todoModel.update({ _id: req.params.id }, { name: req.body.name })
+    .then( function(){
+      res.status(204).send('The TODO was updated.');
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
+});
+
+//delete a todo
+app.delete('/items/:id', (req, res) => {
+  todoModel.remove({ _id: req.params.id })
+    .then( function(){
+      console.log('204 - The TODOs were deleted.');
+      res.status(204).send('The TODOs were deleted.');
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
+});
+
+//show all todos
 app.get('/items', (req, res) => {
   todoModel.find({})
   .then(function(todos) {
-    res.json(todos);  // return all todos in JSON format
     console.log('200 - The TODOs were returned.');
+    res.json(todos);  // return all todos in JSON format
     res.status(200);
   })
-  .catch(err){
-    console.log(err);
-  }
+  .catch(function(err) {console.log(err);});
 });
-
-
-
 
 app.listen(4040, () => {
   console.log('app.js listening on port 4040!')
